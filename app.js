ea71a4df7e44cf1e39a42e9906d78a4958a64eb8
wuky <zz3692177@yahoo.com.tw>
2020-11-30 23:07:33 +0800

    feat: layouts init

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/short-url', {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
+
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main', extname: '.handlebars' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+// 設定 port 3000
+app.listen(3000, () => {
+  console.log('App is running on http://localhost:3000')
+})
+
+
